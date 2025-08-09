837ff7286c91851bc6a5ddee85387b6aa99476e3
Hong Son <hongson530.dev@gmail.com>
2025-08-09 21:30:44 +0700

    chore: initialize

---
+++ src/main/java/com/shongon/smart_budget/SmartBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.shongon.smart_budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shongon/smart_budget/SmartBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shongon.smart_budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
